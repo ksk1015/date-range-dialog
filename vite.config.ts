031ffaadf801de9ddbe9c3e19714c2b8238b9a4a
ksk1015 <ksk.aizawa@gmail.com>
2024-04-04 19:56:10 +0900

    プレビュー用htmlの生成するviteプラグインを自作

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-// import { visualizer } from 'rollup-plugin-visualizer'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,9 +13,24 @@ export default defineConfig({
         ;(window as any)._getDateRangeDialogCssCode = () => cssCode
       },
     }),
-    // visualizer({
-    //   filename: 'dist/stats.html',
-    // }),
+    {
+      name: 'build-preview-html',
+      apply: 'build',
+      closeBundle() {
+        try {
+          // index.html の '/src/main.tsx' を './data-range-dialog.js' に置換して dist/index.html に出力
+          const srcData = fs.readFileSync('index.html', 'utf-8')
+          const distData = srcData.replace(
+            /\/src\/main.tsx/g,
+            './data-range-dialog.js'
+          )
+          fs.writeFileSync('dist/index.html', distData)
+          console.log('dist/index.html を作成')
+        } catch (e) {
+          console.error('dist/index.html の作成に失敗', e)
+        }
+      },
+    },
   ],
   build: {
     outDir: 'dist',
